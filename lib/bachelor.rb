eb2cc8e93cdc88658a8222a3ff4e4d880d2eff99
Jexeones24 <jexeones@gmail.com>
2017-05-24 10:45:39 -0400

    adding code

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,49 @@
+require 'pry'
 def get_first_name_of_season_winner(data, season)
-  # code here
+  season_data = data["#{season}"]
+  winner = season_data.select { |contestant| contestant["status"] == "Winner"}.first
+    winner["name"].partition(" ").first
 end
+
+
+=begin
+def get_contestant_name(data, occupation)
+  data.each do |season, array|
+    array.each do |hash|
+      person = hash.select { |k, v|
+        hash["occupation"].to_s == "#{occupation}"}
+          hash["name"].to_s
+  #        binding.pry
+    end
+  end
+end
+=end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, season_data|
+  person = season_data.select { |k, v|
+    v.to_s == "#{occupation}"}.first
+    person["name"]
+      binding.pry
+    end
+  end
 end
 
+
+#two arguments––the data hash and a string of a hometown. This method should return a counter of the number of contestants who are from that hometown.
+#doesn't count anything
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  data.each do |season, array|
+    array.each_with_object(Hash.new(0)) { |a, h|
+      h[a["#{hometown}"]] += 1}.map {|hometown, count| count}
+  end
 end
+
 
 def get_occupation(data, hometown)
-  # code here
+
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+
 end
